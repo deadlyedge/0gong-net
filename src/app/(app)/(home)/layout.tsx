7756d35089defccfcecadebf16100e87e834a46c
xdream oldlu <xdream@gmail.com>
2025-04-24 20:19:06 +0800

    04: payload integration

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -7,7 +7,7 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
 	return (
-		<div>
+		<div className="flex flex-col min-h-screen">
 			<Navbar />
 			<div className="flex-1 bg-[#f4f4f0]">{children}</div>
 			<Footer />
