7b7ec7c7eca5bab9880c45d5a5c71094ed3b2838
xdream oldlu <xdream@gmail.com>
2025-04-25 12:39:47 +0800

    minor fix

--- src/app/(app)/(home)/search-filters/subcategory-menu.tsx
+++ src/app/(app)/(home)/search-filters/subcategory-menu.tsx
@@ -28,7 +28,7 @@ export const SubcategoryMenu = ({
 				style={{ backgroundColor }}
 				className="w-60 text-black rounded-md overflow-hidden border shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[2px] -translate-y-[2px]">
 				<div>
-					{category.subcategories?.map((subcategory: Category) => (
+					{category.subcategories.map((subcategory: Category) => (
 						<Link
 							key={subcategory.slug}
 							href={`/${category.slug}/${subcategory.slug}`}
