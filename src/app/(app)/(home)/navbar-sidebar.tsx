3fd75759ab1e100cf52b45a48a9ab8ef30c6a1a6
xdream oldlu <xdream@gmail.com>
2025-04-26 10:32:04 +0800

    fix mobile navbar

--- src/app/(app)/(home)/navbar-sidebar.tsx
+++ src/app/(app)/(home)/navbar-sidebar.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link"
+import { useTRPC } from "@/trpc/client"
+import { useQuery } from "@tanstack/react-query"
 
 import {
 	Sheet,
@@ -24,6 +26,9 @@ export const NavbarSidebar = ({
 	open,
 	onOpenChange,
 }: NavbarSidebarProps) => {
+	const trpc = useTRPC()
+	const session = useQuery(trpc.auth.session.queryOptions())
+
 	return (
 		<Sheet open={open} onOpenChange={onOpenChange}>
 			<SheetContent side="left" className="p-0 transition-none">
@@ -40,18 +45,29 @@ export const NavbarSidebar = ({
 							{item.children}
 						</Link>
 					))}
-					<div className="border-t">
-						<Link
-							href="/sign-in"
-							className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center font-medium">
-							Log in
-						</Link>
-						<Link
-							href="/sign-up"
-							className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center font-medium">
-							Start selling
-						</Link>
-					</div>
+
+					{session.data?.user ? (
+						<div className="border-t">
+							<Link
+								href="/admin"
+								className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center font-medium">
+								Dashboard
+							</Link>
+						</div>
+					) : (
+						<div className="border-t">
+							<Link
+								href="/sign-in"
+								className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center font-medium">
+								Log in
+							</Link>
+							<Link
+								href="/sign-up"
+								className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center font-medium">
+								Start selling
+							</Link>
+						</div>
+					)}
 				</ScrollArea>
 			</SheetContent>
 		</Sheet>
