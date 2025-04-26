64dce45687caa2547b026fe38690bf669cdb2426
xdream oldlu <xdream@gmail.com>
2025-04-26 20:21:45 +0800

    12: Filtering products fix 2

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -15,16 +15,13 @@ export const productsRouter = createTRPCRouter({
 		.query(async ({ ctx, input }) => {
 			const where: Where = {}
 
-			if (input.minPrice) {
-				where.price = {
-					...where.price,
-					greater_than_equal: input.minPrice,
+			if (input.minPrice || input.maxPrice) {
+				where.price = {}
+				if (input.minPrice) {
+					where.price.greater_than_equal = input.minPrice
 				}
-			}
-			if (input.maxPrice) {
-				where.price = {
-					...where.price,
-					less_than_equal: input.maxPrice,
+				if (input.maxPrice) {
+					where.price.less_than_equal = input.maxPrice
 				}
 			}
 
